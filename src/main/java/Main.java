55f5b3927e122957657c9fc80a3bb64f0a169480
kazyu0 <kazumakimijima.sb@gmail.com>
2025-03-13 05:17:51 +0000

    if

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,20 +4,10 @@
 
  class Main {
   public static void main(String[] args) {
-    ArrayList<Integer> scores = new ArrayList<Integer>();
+    int value = 3;
 
-    scores.add(1);
-    scores.add(5);
-    scores.add(10);
-    scores.add(15);
-
-    System.out.println(scores.get(0));
-    System.out.println(scores.get(1));
-    System.out.println(scores.get(2));
-    System.out.println(scores.get(3));
+    if (value > 0){
+      System.out.println("値は正です"); 
+    }
   ｝
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
 }
