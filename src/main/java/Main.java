8feaf37e04c3ba585c8483eec33d618992710773
kazyu0 <kazumakimijima.sb@gmail.com>
2025-03-13 05:15:35 +0000

    配列

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,18 @@
 
  class Main {
   public static void main(String[] args) {
-    System.out.println(1000 + 2000);
-    System.out.println(3000 - 1500);
-    System.out.println(50 * 40);
-    System.out.println(600 / 15);
-    System.out.println(5 % 2);
-  }
+    ArrayList<Integer> scores = new ArrayList<Integer>();
 
+    scores.add(1);
+    scores.add(5);
+    scores.add(10);
+    scores.add(15);
+
+    System.out.println(scores.get(0));
+    System.out.println(scores.get(1));
+    System.out.println(scores.get(2));
+    System.out.println(scores.get(3));
+  ｝
   // @Test
   // void addition() {
   //     assertEquals(2, 1 + 1);
